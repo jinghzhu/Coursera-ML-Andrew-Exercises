6e94b4a924081a1de1c2ac56cfa93f9cb0cfba77
Jinghua Zhu <jhzhu@outlook.com>
2019-02-28 21:54:29 +0800

    Predict for one vs all in neural network

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -38,12 +38,12 @@ grad = zeros(size(theta));
 
 h_theta = sigmoid(X * theta);
 
-J = (1/m) * (((-1 * y') * log(h_theta)) - ((1-y') * log(1-h_theta))) + (lambda/(2*m)) * (sum(theta(2:end) .^ 2));
+J = 1/m * (-1 * y') * log(h_theta) - (1 - y') * log(1 - h_theta) + lambda/(2 * m) * (sum(theta(2:end) .^ 2));
 
 temp = theta;
 temp(1) = 0;
 
-grad = ((1 / m) * X' * (h_theta - y)) + (lambda / m) * temp;
+grad = 1/m * X' * (h_theta - y) + lambda / m * temp;
 
 % =============================================================
 
--- ex3/predictOneVsAll.m
+++ ex3/predictOneVsAll.m
@@ -30,13 +30,13 @@ X = [ones(m, 1) X];
 %       for each row.
 %       
 
+predict = sigmoid(X * all_theta'); % 5000 * 401 by 401*10
 
-
-
-
-
+% M = max(A,[],dim) returns the largest elements along dimension dim.
+% For example, if A is a matrix, then max(A,[],2) is a column vector 
+% containing the maximum value of each row.
+[~, p] = max(predict, [], 2);
 
 % =========================================================================
-
 
 end
