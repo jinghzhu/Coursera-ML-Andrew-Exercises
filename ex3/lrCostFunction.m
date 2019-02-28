c687d1935e7e0b5cf8f761e69c17af3301627cdb
Jinghua Zhu <jhzhu@outlook.com>
2019-02-28 21:42:43 +0800

    Vectorizing regularized logistic regression

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -36,14 +36,14 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+h_theta = sigmoid(X * theta);
 
+J = (1/m) * (((-1 * y') * log(h_theta)) - ((1-y') * log(1-h_theta))) + (lambda/(2*m)) * (sum(theta(2:end) .^ 2));
 
+temp = theta;
+temp(1) = 0;
 
-
-
-
-
-
+grad = ((1 / m) * X' * (h_theta - y)) + (lambda / m) * temp;
 
 % =============================================================
 
